8dc782de9d3410fb01548f4442b8816d0a17387d
KASABITI <aggieksbt@gmail.com>
2024-10-09 01:15:09 +0200

    Add root route to prevent 404 error

--- server/index.js
+++ server/index.js
@@ -1,34 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { graphqlHTTP } from 'express-graphql';
-import schema  from './schema.js';
-import connectDB from './db.js';
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema.js";
+import connectDB from "./db.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
-connectDB()
+connectDB();
 
 const port = process.env.PORT || 9000;
 
-app.use(cors())
-app.use('/graphql', graphqlHTTP({
+app.use(cors());
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}));
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}!`);
+// Root route to handle '/'
+app.get("/", (req, res) => {
+  res.send("Welcome to the Todo GraphQL API!");
 });
 
-
-
-
-
-
-
-
-
-
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
